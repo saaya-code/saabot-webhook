10a3396d9cdef4befbb8ed8b6bca0f703e53ec0f
Glitch (hello-express) <none>
2021-04-22 12:02:22 +0000

    🐩⏳ Checkpoint ./server.js:29045346/838 ./package.json:29045346/2743

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const Topgg = require('@top-gg/sdk')
-
 const app = express() 
-
+const axios = require("axios")
 const webhook = new Topgg.Webhook("hunyan") 
-
 app.post('/dblwebhook', webhook.listener(vote => {
-  console.log(vote.user) 
+ axios.post(process.env.web, {
+   content:`<@${vote.user}> voted yar7em waldih`
+ })
 })) 
 
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+
