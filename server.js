fa86e2412c89703fcd81cca0629f105a1422a8c0
Glitch (hello-express) <none>
2021-05-04 13:01:08 +0000

    📇🏟 Checkpoint ./server.js:29045346/222

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const Topgg = require('@top-gg/sdk')
 const app = express() 
 const axios = require("axios")
 const webhook = new Topgg.Webhook("hunyan") 
-var randomColor = Math.floor(Math.random()*16777215).toString(16);
 
 app.post('/dblwebhook', webhook.listener(vote => {
+var c = "0x" + Math.floor(Math.random()*16777215).toString(16);
  axios.post(process.env.web, {
    embeds:[{
       description:`<@${vote.user}> just voted for the bot!`,
-      color: 0xffe700
+      color: Number(c)
    }
    ]
    
